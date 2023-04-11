d53b92bb9e8f66c0605b91f456ce9a1089fbb488
Shweta Kumari <89989861+sanvi0071@users.noreply.github.com>
2023-04-11 12:16:39 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+const [gameTitle, setGameTitle]= useState('');
+const [searchedGames, setSearchedGames] = useState([])
+const [gameDeals, setGameDeals] = useState([])
+
+const searchGame =()=>{
+  fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
+  
+  .then((response)=>response.json())
+  .then((data)=>{
+    setSearchedGames(data)
+    console.log(data)
+  });
+}
+
+useEffect(()=>{
+  fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3`)
+  
+  .then((response)=>response.json())
+  .then((data)=>{
+    setGameDeals(data);
+})
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='searchSection'>
+        <h1>Search for a Game</h1>
+        <input type='text' placeholder='Minecraft..' onChange={(event)=>{setGameTitle(event.target.value)}}/>
+        <button onClick={searchGame}>Search Game Title</button>
+        <div className='games'>
+          {searchedGames.map((game,key)=>{
+            return <div className='game' key={key}>
+              {game.external}
+              
+              <img src={game.thumb}/>
+              {game.cheapest}
+              </div>
+          })}
+        </div>
+
+      </div>
+      <div className='dealsSection'>
+        <h1>Latest Deals</h1>
+        <div className='games'>
+          {gameDeals.map((game,key)=>{
+            return <div className='game' id="deals" key={key}>
+            <h3>{game.title}</h3>  
+              <p>Normal Price: {game.normalPrice}</p>
+             <p>Deal Price: {game.salePrice}</p>
+             <h3>YOU SAVE {game.savings.substr(0,2)}%</h3>
+              </div>
+          })}
+        </div>
+
+      </div>
+      </div>
   );
 }
 
